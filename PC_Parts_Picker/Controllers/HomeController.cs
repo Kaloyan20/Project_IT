a59bbb39809d64a670a740f5979fb43142b55709
Kaloyan20 <k.demirev2006@gmail.com>
2025-02-28 12:06:10 +0200

    k

--- PC_Parts_Picker/Controllers/HomeController.cs
+++ PC_Parts_Picker/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Data;
 using Microsoft.AspNetCore.Mvc;
 using PC_Parts_Picker.Models;
+using Data.Data;
+using System.Linq;
 
 namespace PC_Parts_Picker.Controllers;
 
@@ -27,6 +29,54 @@ public class HomeController : Controller
     public IActionResult Components()
     {
         return View();
+    }
+
+    public IActionResult Cpus()
+    {
+        var cpus = context.Cpus.ToList();
+        return View(cpus);
+    }
+
+    public IActionResult Motherboards()
+    {
+        var motherboards = context.Motherboards.ToList();
+        return View(motherboards);
+    }
+
+    public IActionResult Gpus()
+    {
+        var gpus = context.Gpus.ToList();
+        return View(gpus);
+    }
+
+    public IActionResult Rams()
+    {
+        var rams = context.Rams.ToList();
+        return View(rams);
+    }
+
+    public IActionResult Ssds()
+    {
+        var ssds = context.Ssds.ToList();
+        return View(ssds);
+    }
+
+    public IActionResult Psus()
+    {
+        var psus = context.Psus.ToList();
+        return View(psus);
+    }
+
+    public IActionResult Cases()
+    {
+        var cases = context.Cases.ToList();
+        return View(cases);
+    }
+
+    public IActionResult Coolers()
+    {
+        var coolers = context.Coolers.ToList();
+        return View(coolers);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
